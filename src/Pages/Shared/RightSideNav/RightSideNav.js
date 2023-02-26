ef451e4b6786e6f48361257c464534a04ce1bac9
sowmik-sec <sowmik.sec@gmail.com>
2023-02-26 18:37:22 +0600

    implement right side component and right icons

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,48 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaWhatsapp,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
 
 const RightSideNav = () => {
   return (
     <div>
-      <h2>Right Side Nav</h2>
+      <ButtonGroup vertical>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Log in with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub /> Log in with Github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-5">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaYoutube /> Youtube
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> Whatsapp
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel />
+      </div>
     </div>
   );
 };
