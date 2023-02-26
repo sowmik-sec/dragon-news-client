8b7e8938a4d9c4b24ef6c8b8416d5629ae7faa47
sowmik-sec <sowmik.sec@gmail.com>
2023-02-26 18:59:39 +0600

    navbar with responsive content toggle

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -12,7 +12,7 @@ const Main = () => {
       <Header />
       <Container>
         <Row>
-          <Col lg="2">
+          <Col lg="2" className="d-none d-lg-block">
             <LeftSideNav />
           </Col>
           <Col lg="7">
