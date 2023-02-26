1141f314484040975fde0f4f4a162f0a838969ec
sowmik-sec <sowmik.sec@gmail.com>
2023-02-26 16:05:42 +0600

    create layout, folder structure and basic component

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,4 +1,7 @@
 import Main from "../../layout/Main";
+import Category from "../../Pages/Category/Category/Category";
+import Home from "../../Pages/Home/Home/Home";
+import News from "../../Pages/News/News/News";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -6,6 +9,10 @@ export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    children: [],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/category/:id", element: <Category /> },
+      { path: "/news/:id", element: <News /> },
+    ],
   },
 ]);
