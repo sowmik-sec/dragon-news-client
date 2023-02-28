e66954f410a1670fefa4dc5ba019a7e85dce4733
sowmik-sec <sowmik.sec@gmail.com>
2023-02-28 16:06:47 +0600

    implement google sign in and display user info

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,10 +1,23 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
-  const user = { name: "Habib" };
-  const AuthInfo = { user };
+  const [user, setUser] = useState(null);
+  const providerLogin = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unSubscribe;
+  }, []);
+  const AuthInfo = { user, providerLogin };
   return (
     <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
   );
