faeb8f7c9e9623a9251d7b95480131f175c3463c
sowmik-sec <sowmik.sec@gmail.com>
2023-02-28 12:43:30 +0600

    news loaded by all and category

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const News = () => {
+  const news = useLoaderData();
   return (
     <div>
-      <h2>This is news</h2>
+      <h2>This is news {news.length}</h2>
     </div>
   );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -10,8 +10,17 @@ export const routes = createBrowserRouter([
     path: "/",
     element: <Main />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/category/:id", element: <Category /> },
+      {
+        path: "/",
+        element: <Home />,
+        loader: () => fetch(`http://localhost:5000/news`),
+      },
+      {
+        path: "/category/:id",
+        element: <Category />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/category/${params.id}`),
+      },
       { path: "/news/:id", element: <News /> },
     ],
   },
