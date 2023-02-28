4e997f845748c4db08c9f0c13bf6761dbe77edec
sowmik-sec <sowmik.sec@gmail.com>
2023-02-28 15:04:32 +0600

    display news details

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import NewsSummaryCard from "../../Shared/NewsSummaryCard/NewsSummaryCard";
 
 const Category = () => {
-  const news = useLoaderData();
+  const categoryNews = useLoaderData();
   return (
     <div>
-      <h2>This is Category. {news.length}</h2>
+      <h2>This is Category. {categoryNews.length}</h2>
+      {categoryNews.map((news) => (
+        <NewsSummaryCard key={news._id} news={news} />
+      ))}
     </div>
   );
 };
